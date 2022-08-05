a01fa53fc9fbfc0d8efd104e64b6fff6e3794628
mhevyk <maksimgevyk@gmail.com>
2022-08-05 14:24:59 +0300

    removed previous number extension, added extension tostandardform for big class, that can display not very big or very small number without standard from, can skip 1 if it is not necessary and set fixed precision for very big or very small numbers. also added two array extensions: getuniqueelements and createdeepcopy, because was found a problem, that array, copied with slice leaves object data as reference, so it changes data, but it shouldn	. deep copy creates using json methods stringify and parse

--- js/propotypeExtension.js
+++ js/propotypeExtension.js
@@ -1,45 +1,42 @@
-Number.prototype.roundTo = function(precision){
-	if(!Number.isInteger(precision)) return new Error(`Precision '${precision}' in not an integer!`);
-	//round number and remain only integer part
-	if(precision <= 0){
-		return Math.round(this);
-	}
-	//round number due to precision value
-	const precisionCoefitient = Math.pow(10, precision);
-	return (Math.round(this * precisionCoefitient)) / precisionCoefitient;
+Array.prototype.getUniqueElements = function(){
+	return Array.from(new Set(this));
 }
-Number.prototype.toStandardForm = function(precision){
-	const sign = (Math.sign(this) === -1) ? "-" : "";
-	//get absolute value, because toExponential() method to not work with negative numbers
-	const absoluteNumber = Math.abs(this);
-	//lowerCase, because number can contain 'e' or 'E', it becomes easier to catch needed symbol 'e'
-	const exponentialForm = absoluteNumber.toExponential().toLowerCase();
-
-	//mapping with parseFloat() to convert it to floats
-	let [mantissa, exponent] = exponentialForm.split("e").map(part => parseFloat(part));
-
-	if(Number.isInteger(precision) && precision >= 0){
-		//cut off all numbers after precision + 1, because we round number at (precision) position using (precision+1) position
-		//also we cut off number, because extra digits, like 1.000000000001 can influence rounding
-		const cutMantissa = mantissa.toFixed(precision + 1);
-
-		//get integer part of mantissa and check if it has more than 1 digit
-		const integerMantissa = cutMantissa.slice(0, cutMantissa.indexOf("."));
-		const integerMantissaShift = integerMantissa.length;
-
-		//if integer mantissa has more than one digit, we generate 10 ^ integerMantissaShift - 1 shift to mantissa
-		const extraShift = (integerMantissaShift > 1) ? Math.pow(10, integerMantissaShift - 1) : 1;
-		//shift comma, then round to precision, then fill rest space with zeror
-		mantissa = (parseFloat(cutMantissa) / extraShift).roundTo(precision).toFixed(precision);
-		if(integerMantissaShift > 1){
-			exponent += extraShift - 1;
-		}
-	}
-
-	//if shift is 0, there is no point in showing it, because 10 ^ 0 = 1
-	exponent = exponent ? ` x 10 <sup>${exponent}</sup>` : ``;
-	return sign + mantissa + exponent;
+Array.prototype.createDeepCopy = function(){
+	return JSON.parse(JSON.stringify(this));
 }
-String.prototype.removeAt = function(index){
-	return this.slice(0, index) + this.slice(index + 1);
+Big.prototype.toStandardForm = function(precision){
+	//NORMAL DISPLAY CONSTANTS
+	//numbers in range from (10 ^ -ND_EXPONENT) to (10 ^ ND_EXPONENT) do not display in standard form
+	const ND_EXPONENT = 4;
+	//numbers, which mantissa`s length is less than ND_MANTISSA do not display in standard form
+	const ND_MANTISSA = ND_EXPONENT + 2;
+
+	const exponential = this.toExponential();
+
+	//split exponential from using `e` symbol, because Big.toExponential() incudes only lowercase e
+	let [stringMantissa, stringExponent] = exponential.split("e");
+
+	//convert exponent to int to remove `+` sign before number
+	const numericExponent = parseInt(stringExponent);
+
+	//condition to show number without standard form
+	if(Math.abs(numericExponent) <= ND_EXPONENT && stringMantissa.length <= ND_MANTISSA){
+		return this.toNumber();
+	}
+	/*if number is written in form 1 x 10 ^ x, we skip 1 and return 10 ^ x
+	* we don`t use it in printing, because it can cause loss of data
+	* use parseFloat, because if we parseInt stringMantissa, 1.1313 and 1 will be equal to 1, but it is wrong because loss of data
+	*/
+	const numericMantissa = parseFloat(stringMantissa);
+
+	if(numericMantissa === 1){
+		return `10 <sup>${numericExponent}</sup>`;
+	}
+	//convert bigMantissa to Big in order to use Big.toFixed() method
+	if(precision && Number.isInteger(precision)){
+		const bigMantissa = new Big(stringMantissa);
+		stringMantissa = bigMantissa.toFixed(precision);
+	}
+	//if numericExponent equals 0, we do not show it, because 10 ^ 0 = 1, and mantissa * 1 = mantissa
+	return `${stringMantissa} ${numericExponent ? `x 10 <sup>${numericExponent}` : ""}</sup>`;
 }
