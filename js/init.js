7f0857e1f9b41a6e543e006e240471bd2af0b53c
mhevyk <maksimgevyk@gmail.com>
2022-08-11 21:06:00 +0300

    now converters create using foreach, because keys of converter static variable and types and selectors can be created in a loop

--- js/init.js
+++ js/init.js
@@ -1,39 +1,9 @@
 //init tabs
 const menu = new Tabs("main-navigation-tabs");
 
-//init convertes
-const areaConverter = new Converter({
-    type: "area",
-    selector: "#area-converter"
-});
-const lengthConverter = new Converter({
-    type: "length",
-    selector: "#length-converter"
-});
-const temperatureConverter = new Converter({
-    type: "temperature",
-    selector: "#temperature-converter"
-});
-const volumeConverter = new Converter({
-    type: "volume",
-    selector: "#volume-converter"
-});
-const weightConverter = new Converter({
-    type: "weight",
-    selector: "#weight-converter"
-});
-const dataConverter = new Converter({
-    type: "data",
-    selector: "#data-converter"
-});
-const speedConverter = new Converter({
-    type: "speed",
-    selector: "#speed-converter"
-});
-const timeConverter = new Converter({
-    type: "time",
-    selector: "#time-converter"
-});
+//as id`s are same as Converter.measurementUnitGroups, we use forEach to reduce code for creating all converters
+const converterNames = Object.keys(Converter.measurementUnitGroups);
+converterNames.forEach(name => new Converter({type: name, selector: `#${name}-converter`}));
 
 //init mobile menu
 document.querySelector("#mobile-menu").addEventListener("click", () => {
